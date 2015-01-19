9057503eb49239f0fab4d4ff7b697e5a4e0eed7a
Harry Copeman <harry.copeman@gmail.com>
2015-01-19 15:59:05 +0000

    Fixing Broken Links

--- about/index.php
+++ about/index.php
@@ -1,26 +1,8 @@
 <?php
-$title = "About - Harry Copeman";
+$title = "About - Remote";
 $id = "about";
-include ("../include/header.php");
+include ("../_include/header.php");
 ?>
 
-      <div class="span">
-        <div class="loop-12">
-          <div class="loop-12">
-          <div class="text">
-            <h1 class='page-title'>About</h1>
-            <p>I’m Harry Copeman, a product designer from Hull, England. I’m passionate about simplifying and improving the tools that humans interact with everyday. I sweat over pixels, typography and code. If I’m not at my desk, I’ll be riding my bike.<br><br>
 
-            I’m lucky enough to be joining the amazing Facebook as a product designer, next year in London. Previous to this, I was the co-founder of a digital studio we called <a href="http://cyclestudios.co/" target="_blank">Cycle Studios</a>. Feel free to read my<a href="http://dl.dropbox.com/s/w38vg5upwhm74eq/Harry%20Copeman%20R%C3%A9sum%C3%A9.pdf?dl=0"> Résume</a>, and take a look at my <a href="/portfolio">work</a>.<br><br>
-
-            Away from the screen I spend most of my time with my girlfriend Cara, or turning the gears on my bike through the countryside.<br><br>
-
-              Feel free to follow me on <a href="https://twitter.com/harrycopeman">Twitter</a>, or stay up to date with what I’m working on via <a href="https://dribbble.com/harrycopeman">Dribbble</a>. I’m always up for a chat too - <a href="mailto:harry.copeman@gmail.com">harry.copeman@gmail.com</a>.</p>
-
-            <p>Take it easy.</p>
-          </div>
-          </div>
-        </div>
-      </div>
-
-<?php include ("../include/footer.php"); ?>
+<?php include ("../_include/footer.php"); ?>
